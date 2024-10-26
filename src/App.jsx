1916a3b9d84d80835b64781e66bc160b50b0c982
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2024-10-26 11:13:03 +0545

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       <h1>Hello World</h1>
       <p>Programming should be pretty good</p>
+      <h1>React is Programming language built by Facebook. Meta is the company name</h1>
     </div>
   )
 }
